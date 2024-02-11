02fac62439418f0267649baf678ac25a6d858bb2
JesusOrellana <58049396+JesusOrellana@users.noreply.github.com>
2024-02-11 03:32:19 -0300

    configurar desarrollo local micro servicio auth con docker

--- ms-auth/src/app.controller.ts
+++ ms-auth/src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @MessagePattern({ role: 'auth', cmd: 'login' })
+  login(@Body() body: any): string {
+    return `email ${body?.email}`;
+  }
 
   @MessagePattern({ cmd: 'test' })
   getHello(): string {
--- ms-auth/src/app.module.ts
+++ ms-auth/src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db-auth',
+      port: 3306,
+      username: 'user', // Asegúrate de que este sea el usuario que creaste
+      password: '1234', // Usa la contraseña que definiste para tu usuario de MySQL
+      database: 'db-auth', // Nombre de la base de datos que especificaste
+      entities: [], // Especifica tus entidades aquí
+      synchronize: true, // Ten cuidado con esta opción en producción
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
